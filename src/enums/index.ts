4a72e429a719969d6ce0dd7fc0c49de4536c6f5b
Crypto Lazi Eyes <cryptolazieyes@protonmail.com>
2021-12-01 03:14:35 +0700

    Remove Fee enum and computePoolAddress function

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,6 +1,5 @@
 export { ChainId } from './ChainId'
 export { Rounding } from './Rounding'
 export { TradeType } from './TradeType'
-export { Fee } from './Fee'
 export { Environment } from './Environment'
 export { GasLimit } from './GasLimit'
